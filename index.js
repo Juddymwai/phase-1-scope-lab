5b99c809703aef8e8bdce139861bb1453b6d8b29
Judy Mwai <juddiemukami@gmail.com>
2022-06-09 00:17:02 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+// const globalVar = 1;
+
+// function firstFunc() {
+//   const firstVar = 2;
+
+//   return firstVar + globalVar;
+// }
+
+// firstFunc();
+// // => 3
+
+var customerName= "bob";
+
+function upperCaseCustomerName(){
+
+    return customerName = customerName.toUpperCase()    
+}
+// console.log(upperCaseCustomerName());
+
+function setBestCustomer(){
+    bestCustomer='not bob'
+   
+}
+function overwriteBestCustomer(){
+    bestCustomer='maybe bob'
+}
+
+const leastFavoriteCustomer= 'jay'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer='bob'
+}
